8d4bb49deb986936652ad7f4729f48b7e00dd77c
Lucas Ogden-Davis <lucas@ogdendavis.com>
2018-12-04 11:42:18 -0500

    Create login state at route /login/

--- project.py
+++ project.py
@@ -1,3 +1,6 @@
+### This is a course-provided finished version of the menu project ###
+### My edits / additions will be to implement user authentication ###
+
 from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
 app = Flask(__name__)
 
@@ -5,14 +8,29 @@ from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
-
 #Connect to Database and create database session
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
-
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# Imports to implement user authentication
+# session already refers to a database session, so login sessions will be
+# imported as login_session
+from flask import session as login_session
+import string, random
+
+# Path for login
+@app.route('/login/')
+def showLogin():
+    # Create a 32-digit semi-random string of uppercase letters and digits
+    # This will be the unique session key that we will use to confirm that the
+    # requests we get are coming from the user, not a third party
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    # Set the 'random' string to the state property of our login session
+    login_session['state'] = state
+    # For now, just confirm this worked by printing the login_session state
+    return "The current session state is {}".format(login_session['state'])
 
 #JSON APIs to view Restaurant Information
 @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
@@ -84,7 +102,7 @@ def showMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
     return render_template('menu.html', items = items, restaurant = restaurant)
-     
+
 
 
 #Create a new menu item
@@ -116,7 +134,7 @@ def editMenuItem(restaurant_id, menu_id):
         if request.form['course']:
             editedItem.course = request.form['course']
         session.add(editedItem)
-        session.commit() 
+        session.commit()
         flash('Menu Item Successfully Edited')
         return redirect(url_for('showMenu', restaurant_id = restaurant_id))
     else:
@@ -127,7 +145,7 @@ def editMenuItem(restaurant_id, menu_id):
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete', methods = ['GET','POST'])
 def deleteMenuItem(restaurant_id,menu_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-    itemToDelete = session.query(MenuItem).filter_by(id = menu_id).one() 
+    itemToDelete = session.query(MenuItem).filter_by(id = menu_id).one()
     if request.method == 'POST':
         session.delete(itemToDelete)
         session.commit()
